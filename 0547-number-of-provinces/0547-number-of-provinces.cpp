32a0d65979f42d1caf43f2b280aba626911cda6d
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-05 21:46:27 +0530

    Time: 3 ms (46.14%), Space: 20 MB (41.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    void BFS(int node,vector<int>&vis,vector<vector<int>>& iC)
-    {
-        queue<int>q;
-        q.push(node);
-        while(!q.empty())
-        {
-            int src=q.front();
-            q.pop();
-            vis[src]=1;
-            for(int i=0;i<iC.size();i++)
-            {
-                
-                if(iC[src-1][i]==1 && vis[i+1]==0)
-                {
-                    vis[i+1]=1;
-                    q.push(i);
-                }
+   void BFS(int node, vector<int>& vis, vector<vector<int>>& iC) {
+    queue<int> q;
+    q.push(node);
+    vis[node] = 1; // Mark the node as visited
+    while (!q.empty()) {
+        int src = q.front();
+        q.pop();
+        for (int i = 0; i < iC.size(); i++) {
+            if (iC[src][i] == 1 && vis[i] == 0) { // 0-based indexing
+                vis[i] = 1;
+                q.push(i);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& iC) 
-    {
-        int ans=0;
-        int n=iC.size();
-        vector<int>vis(n+1,0);
-        for(int i=1;i<n+1;i++)
-        {
-            if(vis[i]==0)
-            {
-                vis[i]=1;
-                BFS(i,vis,iC);
-                ans++;
-            }
+}
+
+int findCircleNum(vector<vector<int>>& iC) {
+    int ans = 0;
+    int n = iC.size();
+    vector<int> vis(n, 0); // Use size 'n' for 0-based indexing
+    for (int i = 0; i < n; i++) { // 0-based indexing
+        if (vis[i] == 0) {
+            BFS(i, vis, iC);
+            ans++;
         }
-        return ans;
     }
+    return ans;
+}
+
 };
