04da0ccb286cfde25143362321e61ec3d879aa32
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-07 15:38:44 +0530

    Time: 34 ms (14.07%), Space: 23 MB (5.19%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) 
     {
-        // Handle edge case where k is negative
         if (k < 0) return 0;
 
         multiset<int> mst(nums.begin(), nums.end());
         set<pair<int, int>> st;
 
-        for (int num : nums) {
-            mst.erase(mst.find(num));  // Remove current element for finding pairs
+        for (int num : nums) 
+        {
+            mst.erase(mst.find(num));
 
-            // Look for pair num + k
-            if (mst.find(num + k) != mst.end()) {
+            if (mst.find(num + k) != mst.end()) 
+            {
                 st.insert({num, num + k});
             }
 
-            // Look for pair num - k
-            if (mst.find(num - k) != mst.end()) {
+            if (mst.find(num - k) != mst.end()) 
+            {
                 st.insert({num - k, num});
             }
 
