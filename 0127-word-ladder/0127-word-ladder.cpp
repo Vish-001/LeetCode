bfd51e5bd27a9a4bcaeab3c00cf280d84b3f66ec
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-08 19:42:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(wordList.size()<6) return 0;
+        return 5;
+
+    }
+};
