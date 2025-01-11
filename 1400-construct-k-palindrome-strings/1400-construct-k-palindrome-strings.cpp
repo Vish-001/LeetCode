514ac001aa7d5cf23649bc0d627e8a5a3d81c7f1
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-11 05:58:45 +0530

    Time: 10 ms (43.36%), Space: 15.1 MB (21.68%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    bool isPalindrome(string s) 
-    {
-        int i = 0, j = s.size() - 1;
-        while (i < j) 
-        {
-            if (s[i] != s[j]) 
-            {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-
     bool canConstruct(string s, int k) 
     {
         if (k > s.size()) return false;
