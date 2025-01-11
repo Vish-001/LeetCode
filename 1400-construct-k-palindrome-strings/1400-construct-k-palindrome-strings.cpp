ac95116deff1a123f7576eda015c443607f0bfe3
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-11 05:56:51 +0530

    Time: 15 ms (27.27%), Space: 15.2 MB (21.68%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPalindrome(string s) 
+    {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    bool canConstruct(string s, int k) 
+    {
+        if (k > s.size()) return false;
+
+        unordered_map<char, int> freq;
+        for (char c : s) 
+        {
+            freq[c]++;
+        }
+
+        int oddCount = 0;
+        for (auto& [ch, count] : freq) 
+        {
+            if (count % 2 != 0) 
+            {
+                oddCount++;
+            }
+        }
+
+        return oddCount <= k;
+    }
+};
