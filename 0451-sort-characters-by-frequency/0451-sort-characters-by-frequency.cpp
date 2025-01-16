48dd35a1903ab7bdb2e3f7933dbd3e06b48259ca
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-16 05:57:46 +0530

    Time: 3 ms (57.96%), Space: 11.4 MB (38.67%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,27 +1,36 @@
 class Solution {
-    static bool comparator(pair<int, char> p1, pair<int, char> p2) {
-        if (p1.first > p2.first) {
-            return true;
-        }
-        if (p1.first < p2.first) {
+    static bool comparator(pair<char,int> p1, pair<char,int> p2) 
+    {
+        if(p1.second < p2.second)
+        {
             return false;
         }
-        return p1.second < p2.second;
+        if(p1.second > p2.second)
+        {
+            return true;
+        }
+
+        return p1.first > p2.first;
     }
 public:
-    string frequencySort(string s) {
+    string frequencySort(string s) 
+    {
         unordered_map<char, int> freq;
-        for (char ch : s) {
+        for (char ch : s) 
+        {
             freq[ch]++;
         }
-        vector<pair<int, char>> freqVec;
-        for (auto &entry : freq) {
-            freqVec.push_back({entry.second, entry.first});
+        vector<pair<char,int>> freqVec;
+        for (auto &entry : freq) 
+        {
+            freqVec.push_back({entry.first, entry.second});
         }
+
         sort(freqVec.begin(), freqVec.end(), comparator);
         string ans;
-        for (auto &entry : freqVec) {
-            ans.append(entry.first, entry.second);
+        for (auto &entry : freqVec) 
+        {
+            ans.append(entry.second, entry.first);
         }
         return ans;
     }
