3bb6d51eb66f8e01f7818817229f20cda318b0ed
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-16 16:05:46 +0530

    Time: 4 ms (54.32%), Space: 118 MB (68.31%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,59 +1,41 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+
+class Solution
+{
 public:
-    ListNode*Rev(ListNode*head)
+    ListNode* Rev(ListNode*&temp)
     {
-        if(head->next==nullptr) return head;
-
+        ListNode*curr=temp;
         ListNode*prev=nullptr;
-        ListNode*curr=head;
-        ListNode*nxt=nullptr;
-
-        while(curr!=nullptr)
+        while(curr->next)
         {
-            nxt=curr->next;
+            ListNode*nxt=curr->next;
             curr->next=prev;
             prev=curr;
             curr=nxt;
         }
-        return prev;
-
+        curr->next=prev;
+        return curr;
     }
 
     bool isPalindrome(ListNode* head) 
     {
         if(head==nullptr || head->next==nullptr) return true;
-
         ListNode*slow=head;
         ListNode*fast=head;
 
-        while(fast->next!=nullptr && fast->next->next!=nullptr)
+        while(fast && fast->next)
         {
             slow=slow->next;
             fast=fast->next->next;
         }
-
-        ListNode*newH=Rev(slow->next);
-
-        ListNode*dummy=head;
-
-        while(newH!=nullptr)
+        slow=Rev(slow);
+        fast=head;
+        while(fast->next)
         {
-            if(newH->val!=dummy->val) return false;
-            newH=newH->next;
-            dummy=dummy->next;
+            if(fast->val!=slow->val) return false;
+            fast=fast->next;
+            slow=slow->next;
         }
-        
         return true;
-
     }
 };
