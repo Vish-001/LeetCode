4123d7cc8b81f3725e7475b6aa05c3f084a7f813
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-16 12:38:56 +0530

    Time: 3 ms (91.49%), Space: 14.9 MB (90.01%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,47 +1,69 @@
-class Solution 
-{
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
 public:
     Node* copyRandomList(Node* head) 
     {
-        if(head == nullptr) return head;
-        Node* temp = head;
-        Node* newH = nullptr;
+        if(head==nullptr) return head;
 
-        while (temp != nullptr) 
+        Node*newH=nullptr;
+        Node*temp=head;
+
+        // add dupli
+        while(temp!=nullptr)
         {
-            Node* T = new Node(temp->val);
-            if (newH == nullptr) newH = T;
-            T->next = temp->next;
-            temp->next = T;
-            temp = temp->next->next;
+            Node*T=new Node(temp->val);
+            if(newH==nullptr) newH=T;
+            T->next=temp->next;
+            temp->next=T;
+            temp=temp->next->next;
         }
 
-        temp = head;
-        Node* T = newH;
-        while (temp != nullptr) 
+        // copy random
+        temp=head;
+        Node*T=newH;
+
+        while(temp!=nullptr)
         {
-            if(temp->random == nullptr) 
+            if(temp->random==nullptr)
             {
-                T->random = nullptr;
-            } 
-            else 
-            {
-                T->random = temp->random->next;
+                T->random=nullptr;
+
             }
-            temp = T->next;
-            if(temp != nullptr) T = temp->next;
+            else
+            {
+                T->random=temp->random->next;
+            }
+            temp=temp->next->next;
+            if(temp!=nullptr)T=temp->next;
         }
 
-        T = newH;
-        temp = head;
-        while (temp != nullptr) 
+        // remove connection with og
+
+        temp=head;
+        T=newH;
+
+        while(temp!=nullptr)
         {
-            temp->next = T->next;
-            if (temp->next != nullptr) T->next = temp->next->next;
-            temp = temp->next;
-            if (temp != nullptr) T = temp->next;
+            temp->next=T->next;
+            if(temp->next!=nullptr)T->next=temp->next->next;
+            temp=temp->next;
+            if(temp!=nullptr)T=temp->next;
         }
-
         return newH;
     }
 };
