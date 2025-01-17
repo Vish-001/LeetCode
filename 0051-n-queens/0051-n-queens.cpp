c4a50978b7cb7320505f145e20daac0a773e592c
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-17 14:50:09 +0530

    Time: 3 ms (65.73%), Space: 10.3 MB (50.15%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    void Func(int col, vector<string>& board, vector<int>& rowHash, vector<int>& upperDiag, vector<int>& lowerDiag, int n, vector<vector<string>>& ans) {
-        if (col == n) {
+    void Func(int col, vector<string>& board, vector<int>& rowHash, vector<int>& upperDiag, vector<int>& lowerDiag, int n, vector<vector<string>>& ans) 
+    {
+        if (col == n) 
+        {
             ans.push_back(board);
             return;
         }
 
-        for (int row = 0; row < n; row++) {
-            if (rowHash[row] == 0 && upperDiag[row + col] == 0 && lowerDiag[(n - 1) + (row - col)] == 0) {
-                // Place the queen
+        for (int row = 0; row < n; row++) 
+        {
+            if (rowHash[row] == 0 && upperDiag[row + col] == 0 && 
+            lowerDiag[(n - 1) + (row - col)] == 0) 
+            {
                 rowHash[row] = 1;
                 upperDiag[row + col] = 1;
-                lowerDiag[(n - 1) + (row - col)] = 1;
+                lowerDiag[(row+n)-col-1] = 1;
                 board[row][col] = 'Q';
 
-                // Recur to the next column
                 Func(col + 1, board, rowHash, upperDiag, lowerDiag, n, ans);
 
-                // Backtrack
                 rowHash[row] = 0;
                 upperDiag[row + col] = 0;
-                lowerDiag[(n - 1) + (row - col)] = 0;
+                lowerDiag[(row+n)-col-1] = 0;
                 board[row][col] = '.';
             }
         }
     }
 
-    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n) 
+    {
         vector<vector<string>> ans;
-        vector<string> board(n, string(n, '.')); // Initialize an empty board
-        vector<int> rowHash(n, 0);               // Hash to track rows
-        vector<int> upperDiag(2 * n - 1, 0);     // Hash to track upper diagonals
-        vector<int> lowerDiag(2 * n - 1, 0);     // Hash to track lower diagonals
+        vector<string> board(n, string(n, '.')); 
+        vector<int> rowHash(n, 0);              
+        vector<int> upperDiag(2 * n - 1, 0);     
+        vector<int> lowerDiag(2 * n - 1, 0);     
 
         Func(0, board, rowHash, upperDiag, lowerDiag, n, ans);
         return ans;
