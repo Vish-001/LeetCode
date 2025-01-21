d39f8f13a95ffcb9b87469771a1739eebcb53caa
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-21 09:16:55 +0530

    Time: 7 ms (67.87%), Space: 10.3 MB (98.1%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string minWindow(string s, string t) 
+    {
+        int r=0;
+        int l=0;
+        int sidx=-1;
+        int cnt=0;
+        int ans=INT_MAX;
+        int hash[256]={0};
+        for(auto c:t)
+        {
+            hash[c]++;
+        }
+
+        while(r<s.size())
+        {
+            if(hash[s[r]]>0)
+            {
+                cnt++;
+            }
+            hash[s[r]]--;
+            r++;
+            while(cnt==t.size())
+            {
+                if(r-l<ans)
+                {
+                    ans=r-l;
+                    sidx=l;
+                }
+                hash[s[l]]++;
+                if(hash[s[l]]>0)
+                {
+                    cnt--;
+                }
+                l++;
+            }
+        }
+        if(sidx==-1)
+        {
+            return "";
+        }
+        return s.substr(sidx,ans);
+    }
+};
