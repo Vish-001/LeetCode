99efea4156e00cf96bc11e40671c4bfcae81b850
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-30 15:21:20 +0530

    Time: 0 ms (100%), Space: 22.1 MB (19.34%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    bool Inorder(TreeNode* root, TreeNode*& prev) {
-        if (root == nullptr) return true;
+    bool Inorder(TreeNode* root, TreeNode*& prev) 
+    {
+        if (root == nullptr) 
+        {
+            return true;
+        }
 
-        if (!Inorder(root->left, prev)) return false;
+        if (!Inorder(root->left, prev)) 
+        {
+            return false;
+        }
 
-        if (prev != nullptr && root->val <= prev->val) return false;
-        prev = root; // Update prev to the current node
+        if (prev != nullptr && root->val <= prev->val) 
+        {
+            return false;
+        }
+
+        prev = root;
 
         return Inorder(root->right, prev);
     }
 
-    bool isValidBST(TreeNode* root) {
-        TreeNode* prev = nullptr; // Use pointer to handle INT_MIN case
+    bool isValidBST(TreeNode* root) 
+    {
+        TreeNode* prev = nullptr;
         return Inorder(root, prev);
     }
 };
