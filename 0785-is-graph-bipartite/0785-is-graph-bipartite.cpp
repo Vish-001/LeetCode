3b7fb4a8623c934366801b3525ea60fda3945068
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-31 23:00:24 +0530

    Time: 0 ms (100%), Space: 17.4 MB (39.33%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -2,38 +2,34 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) 
     {
-        int n = graph.size();
-        vector<int> vis(n, -1);
-        vector<int> color(n, -1);
-
-        for (int start = 0; start < n; start++) 
+        vector<int> vis(graph.size(), -1);
+        vector<int> color(graph.size(), -1);
+        queue<pair<int, int>> q;
+        for(int i=0;i<graph.size();i++)
         {
-            if (vis[start] == -1) // Check all components
+            if(vis[i]==1) continue;
+            q.push({i, 0}); 
+            vis[i] = 1;
+            color[i] = 0;
+
+            while (!q.empty())
             {
-                queue<pair<int, int>> q;
-                q.push({start, 0});
-                vis[start] = 1;
-                color[start] = 0;
+                auto p = q.front();
+                q.pop();
+                int node = p.first;
+                int col = p.second;
 
-                while (!q.empty())
+                for (auto c : graph[node])
                 {
-                    auto p = q.front();
-                    q.pop();
-                    int node = p.first;
-                    int col = p.second;
-
-                    for (auto c : graph[node])
+                    if (vis[c] == -1)
                     {
-                        if (vis[c] == -1)
-                        {
-                            vis[c] = 1;
-                            color[c] = 1 - col;
-                            q.push({c, color[c]});
-                        }
-                        else if (color[c] == col)
-                        {
-                            return false;
-                        }
+                        vis[c] = 1;
+                        color[c] = 1 - col;
+                        q.push({c, color[c]});
+                    }
+                    else if (color[c] == col)
+                    {
+                        return false;
                     }
                 }
             }
