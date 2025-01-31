1ad87b34957a819602589c0e9c9cf8b59eec0ff8
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-01-31 22:55:45 +0530

    Time: 0 ms (100%), Space: 17.5 MB (17.85%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-    // BFS function to check bipartiteness for a connected component
-    bool BFS(int startNode, vector<int>& col, vector<vector<int>>& graph) {
-        queue<pair<int, int>> q;
-        q.push({startNode, 1}); // Start with the first node and color it with 1
-        col[startNode] = 1;
+    bool DFS(int s, vector<int>& col, vector<vector<int>>& graph) 
+    {
+        stack<int> st;
+        st.push(s);
+        col[s] = 0; // Start with color 0
 
-        while (!q.empty()) {
-            auto p = q.front();
-            q.pop();
-            int node = p.first;
-            int clr = p.second;
+        while (!st.empty()) 
+        {
+            int node = st.top();
+            st.pop();
 
-            for (int neighbor : graph[node]) {
-                if (col[neighbor] == -1) {
-                    int nclr = 1 - clr; // Flip color
-                    col[neighbor] = nclr;
-                    q.push({neighbor, nclr});
-                } else if (col[neighbor] == clr) {
-                    return false; // If a neighbor has the same color, it's not bipartite
+            for (int neighbor : graph[node]) 
+            {
+                if (col[neighbor] == -1) 
+                {
+                    col[neighbor] = 1 - col[node]; // Assign opposite color
+                    st.push(neighbor);
+                } 
+                else if (col[neighbor] == col[node]) 
+                {
+                    return false; // Same color detected
                 }
             }
         }
         return true;
     }
 
-    bool isBipartite(vector<vector<int>>& graph) {
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
         int n = graph.size();
         vector<int> col(n, -1); // Color array, -1 indicates uncolored
 
-        for (int i = 0; i < n; i++) {
-            // Check each component in the graph
-            if (col[i] == -1) { // If not colored, start BFS
-                if (!BFS(i, col, graph)) {
-                    return false; // If any component isn't bipartite, return false
+        for (int i = 0; i < n; i++) 
+        {
+            if (col[i] == -1) 
+            { 
+                if (!DFS(i, col, graph)) 
+                {
+                    return false; 
                 }
             }
         }
