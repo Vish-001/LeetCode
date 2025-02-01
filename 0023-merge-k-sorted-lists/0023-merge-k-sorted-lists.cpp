64495bf9d109609de166684eb4d42d6c095bf5b8
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-01 23:28:20 +0530

    Time: 18 ms (24.13%), Space: 16.7 MB (100%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+        int n = lists.size();
+        if (n == 0) {
+            return nullptr;
+        }
+        if (n == 1) {
+            return lists[0];
+        }
+        
+        int interval = 1;
+        while (interval < n) {
+            for (int i = 0; i < n - interval; i += interval * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+        
+        return lists[0];
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) {
+            return list2;
+        }
+        if (!list2) {
+            return list1;
+        }
+
+        ListNode* head;
+        if (list1->val < list2->val) {
+            head = list1;
+            list1 = list1->next;
+        } else {
+            head = list2;
+            list2 = list2->next;
+        }
+
+        ListNode* current = head;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                current->next = list1;
+                list1 = list1->next;
+            } else {
+                current->next = list2;
+                list2 = list2->next;
+            }
+            current = current->next;
+        }
+
+        if (list1) {
+            current->next = list1;
+        } else {
+            current->next = list2;
+        }
+
+        return head;
+    }
+};
