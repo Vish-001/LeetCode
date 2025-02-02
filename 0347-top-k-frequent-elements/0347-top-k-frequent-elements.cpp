2057905fb5bd77c09745059efcc50de58dd6afb2
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-02 17:08:09 +0530

    Time: 5 ms (28.12%), Space: 17.9 MB (72.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,41 +1,40 @@
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <utility>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // Step 1: Calculate the frequency of each element
-        unordered_map<int, int> mp;
-        for (int num : nums) {
-            mp[num]++;
+        unordered_map<int, int> freqMap;
+        for(int num : nums) {
+            freqMap[num]++;
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        unordered_set<int>st;
-        for (auto& it : mp) {
-            pq.push({it.second, it.first});
-            if (pq.size() > k) 
-            {
-                auto t=pq.top();
-                st.insert(t.second);
-                pq.pop(); 
+        // Step 2: Use a min-heap to find the top k frequent elements
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        
+        for(auto& it : freqMap) {
+            minHeap.push({it.second, it.first}); // {frequency, element}
+            if(minHeap.size() > k) {
+                minHeap.pop(); // Remove the least frequent element
             }
         }
-
-        vector<int> ans;
-        for(auto c:nums)
-        {
-            if(st.find(c)==st.end())
-            {
-                ans.push_back(c);
-                st.insert(c);
-            }
+        
+        // Step 3: Collect the top k elements
+        vector<int> result;
+        while(!minHeap.empty()) {
+            result.push_back(minHeap.top().second);
+            minHeap.pop();
         }
-
-        return ans;
+        
+        return result;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
