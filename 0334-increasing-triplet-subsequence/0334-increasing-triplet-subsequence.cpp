8baff108735f09655afda1536f87f1d1ec9f29e9
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-06 23:33:51 +0530

    Time: 0 ms (100%), Space: 115.6 MB (80.59%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        stack<int> st; // Monotonic increasing stack
-        
-        for (int i=nums.size()-1;i>=0;i--) 
+        int first = INT_MAX, second = INT_MAX;
+
+        for (int num : nums) 
         {
-            while (!st.empty() && st.top() <= nums[i]) {
-                st.pop();
-            }
-
-            // Push current element into the stack
-            st.push(nums[i]);
-
-            // If we ever have 3 elements, we found a triplet
-            if (st.size() >= 3) {
-                return true;
+            if (num <= first) 
+            {
+                first = num;
+            } 
+            else if (num <= second) 
+            {
+                second = num;
+            } 
+            else 
+            {
+                return true;  
             }
         }
-        
+
         return false;
     }
 };
