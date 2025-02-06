ec4d20fd5b81c7dadb7845021ad5014fbe6d214d
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-06 07:50:01 +0530

    Time: 0 ms (100%), Space: 8.1 MB (50.36%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,35 +1,33 @@
-#include <string>
-using namespace std;
-
 class Solution {
 public:
-    bool checkValidString(string s) {
-        int mini = 0;  // Minimum possible '(' count
-        int maxi = 0;  // Maximum possible '(' count
-
-        for (auto c : s) {
-            if (c == '(') {
+    bool checkValidString(string s) 
+    {
+        int mini=0;
+        int maxi=0;
+        for(auto c:s)
+        {
+            if(c=='(')
+            {
                 mini++;
                 maxi++;
-            } else if (c == ')') {
+            }
+            else if(c==')')
+            {
+                mini--;
                 maxi--;
-                mini--;
-            } else if (c == '*') {
-                mini--;
+            }
+            else
+            {
                 maxi++;
+                mini--;
             }
-
-            if (mini < 0) {
-                mini = 0;  // Minimum cannot be negative
+            if(mini<0)
+            {
+                mini=0;
             }
-
-            // If maximum possible '(' count is negative, return false
-            if (maxi < 0) {
-                return false;
-            }
+            if(maxi<0) return false;
         }
-
-        // If minimum possible '(' count is zero, string is valid
-        return mini == 0;
+        if(maxi==0 || mini==0) return true;
+        return false;
     }
 };
