501c2a64cbcbcf2792dcb85ce7ce01c85e4f52dd
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-11 01:22:07 +0530

    Time: 50 ms (86.52%), Space: 19.6 MB (92.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,8 +1,41 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        if(wordList.size()<6) return 0;
-        return 5;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
+    {
+        unordered_set<string>wl(wordList.begin(),wordList.end());
+        if(wl.find(endWord)==wl.end()) return 0;
+        queue<string>q;
+        q.push(beginWord);
+        int ans=1;
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                string T=q.front();
+                q.pop();
+                if(T==endWord)
+                {
+                    return ans;
+                }
+                for(int j=0;j<T.size();j++)
+                {
+                    for(char ch='a';ch<='z';ch++)
+                    {
+                        char c=T[j];
+                        T[j]=ch;
+                        if(wl.find(T)!=wl.end())
+                        {
+                            wl.erase(T);
+                            q.push(T);
+                        }
+                        T[j]=c;
+                    }
+                }
+            }
+            ans++;
+        }
 
+        return 0;
     }
 };
