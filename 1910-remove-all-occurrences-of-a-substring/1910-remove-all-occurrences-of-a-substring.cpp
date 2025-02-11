a05227bebdf8da178eb4425590867904f16484aa
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-11 17:30:30 +0530

    Time: 0 ms (100%), Space: 7.1 MB (100%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,51 +1,10 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) 
-    {
-        // Edge-case: if part is empty, simply return s.
-        if(part.empty()) return s;
-        
-        int n = part.size();
-        char last = part[n - 1];
-        stack<char> st;
-
-        for(auto c : s)
-        {
-            if(st.size() < n - 1 || c != last)
-            {
-                st.push(c);
-            }
-            else
-            {
-                int i = n - 2;
-                string t = "";
-                
-                while(i >= 0 && !st.empty() && st.top() == part[i])
-                {
-                    t.insert(t.begin(), st.top());
-                    st.pop();
-                    i--;
-                }
-                
-                if(t + last != part)
-                {
-                    t.push_back(last);
-                    for(auto ch : t)
-                    {
-                        st.push(ch);
-                    }
-                }
-            }
+    string removeOccurrences(string s, string part) {
+        int n = s.length();
+        while(n!=0 && s.find(part)<n){
+            s.erase(s.find(part),part.length());
         }
-        
-        string ans = "";
-        while(!st.empty())
-        {
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        if(ans)
-        return ans;
+        return s;
     }
 };
