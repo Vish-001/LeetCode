cb7819fe9bb4a130190d1c4e32fccdd071f0ea77
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-11 17:20:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -11,43 +11,33 @@ public:
 
         for(auto c : s)
         {
-            // If there are not enough characters in the stack to potentially form "part"
-            // or the current character is not equal to the last character of "part",
-            // simply push the current character.
             if(st.size() < n - 1 || c != last)
             {
                 st.push(c);
             }
             else
             {
-                // At this point, st.size() >= n-1 and c equals the last character of part.
                 int i = n - 2;
                 string t = "";
                 
-                // Try to match the preceding n-1 characters of part from the stack.
                 while(i >= 0 && !st.empty() && st.top() == part[i])
                 {
-                    // Prepend st.top() to t. (std::string does not support push_front.)
                     t.insert(t.begin(), st.top());
                     st.pop();
                     i--;
                 }
                 
-                // If t concatenated with the current character does not form "part",
-                // push the characters back into the stack.
                 if(t + last != part)
                 {
-                    t.push_back(last); // Append the current character.
+                    t.push_back(last);
                     for(auto ch : t)
                     {
                         st.push(ch);
                     }
                 }
-                // Otherwise, we have matched "part", so we remove it by not pushing anything back.
             }
         }
         
-        // Build the final string from the stack.
         string ans = "";
         while(!st.empty())
         {
@@ -55,7 +45,7 @@ public:
             st.pop();
         }
         reverse(ans.begin(), ans.end());
-        
+        if(ans)
         return ans;
     }
 };
