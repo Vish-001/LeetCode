a0e252cbcf34114a1d56cafd0120bb6945aede23
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-12 16:32:54 +0530

    Time: 206 ms (30.55%), Space: 14 MB (27.4%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int dp[301][301]; 
-    
-    int Func(vector<int>& nums, int left, int right) 
-    {
+    int Func(vector<int>& nums, int left, int right, vector<vector<int>>& dp) {
         if (left > right) return 0;
         
         if (dp[left][right] != -1) return dp[left][right]; // Return cached result
 
         int maxi = 0;
         for (int i = left; i <= right; i++) {
-            int coins = nums[left - 1] * nums[i] * nums[right + 1] 
-                        + Func(nums, left, i - 1) + Func(nums, i + 1, right);
+            int coins = (nums[left - 1] * nums[i] * nums[right + 1]) 
+                        + Func(nums, left, i - 1, dp) 
+                        + Func(nums, i + 1, right, dp);
             maxi = max(maxi, coins);
         }
 
-        return dp[left][right] = maxi; // Store result in DP table
+        return dp[left][right] = maxi; 
     }
 
-    int maxCoins(vector<int>& nums) {
-        nums.insert(nums.begin(), 1); // Add virtual left boundary
-        nums.push_back(1); // Add virtual right boundary
-        memset(dp, -1, sizeof(dp)); // Initialize DP table
-        return Func(nums, 1, nums.size() - 2); // Call recursive function
+    int maxCoins(vector<int>& nums) 
+    {
+        nums.insert(nums.begin(), 1); 
+        nums.push_back(1); 
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return Func(nums, 1, n - 2, dp); 
     }
 };
