bc857cd010830997dce928d484413d5b5aad56eb
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-21 00:06:45 +0530

    Time: 15 ms (20.43%), Space: 11.1 MB (8.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k) {
-        int r = 0, l = 0, ans = 0, mf = 0;
-        unordered_map<char, int> mp;
-
-        while (r < s.size()) {
-            mp[s[r]]++;
-            mf = max(mf, mp[s[r]]);
-
-            // If the current window is invalid
-            while ((r - l + 1) - mf > k) {
+    int characterReplacement(string s, int k) 
+    {
+        int ans=0;
+        int max_freq=0;
+        unordered_map<char,int>mp;
+        int l=0;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+            max_freq=max(max_freq,mp[s[i]]);
+            if((i-l+1)-max_freq>k)
+            {
                 mp[s[l]]--;
-                l++;
-
-                // Recalculate mf in the current window
-                mf = 0;
-                for (auto &entry : mp) {
-                    mf = max(mf, entry.second);
+                if(mp[s[l]]==0)
+                {
+                    mp.erase(mp[s[l]]);
                 }
+                l++;
+            }
+            for(auto& p:mp)
+            {
+                max_freq=max(max_freq,p.second);
             }
 
-            ans = max(ans, r - l + 1);
-            r++;
+            ans=max(ans,i-l+1);
         }
-
         return ans;
     }
 };
