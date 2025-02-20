7d4aea17ceb6c5c99896e401e8ee92a49091a463
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-20 17:44:55 +0530

    Time: 50 ms (14.88%), Space: 45.3 MB (13.8%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    void Func(int i,int target,vector<int>ds,vector<int>& candidates,vector<vector<int>>&ans)
+    void Func(int i,vector<int>ds,int target,vector<int>& candidates,vector<vector<int>>&ans)
     {
-        if(target==0)
-        {
-            ans.push_back(ds);
-            return;
-        }
         if(i==candidates.size())
         {
+            if(target==0)
+            {
+                ans.push_back(ds);
+            }
             return;
         }
+
         if(target-candidates[i]>=0)
         {
             ds.push_back(candidates[i]);
-            Func(i,target-candidates[i],ds,candidates,ans);
+            Func(i,ds,target-candidates[i],candidates,ans);
             ds.pop_back();
         }
-        Func(i+1,target,ds,candidates,ans);
+        Func(i+1,ds,target,candidates,ans);
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
     {
         vector<vector<int>>ans;
         vector<int>ds;
-        Func(0,target,ds,candidates,ans);
+        Func(0,ds,target,candidates,ans);
         return ans;
     }
 };
