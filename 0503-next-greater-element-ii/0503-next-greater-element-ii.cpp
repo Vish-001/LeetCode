96bf6630ffdd0b23f81cb2ca9dbeb4df4ac0abb8
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-21 11:40:35 +0530

    Time: 8 ms (28.09%), Space: 29 MB (16.25%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int> st; // Stack to store elements
-        int n = nums.size();
-        vector<int> result(n, -1); // Initialize result with -1
-        
-        // Pre-insert the array into the stack from back
-        for (int i = n - 1; i >= 0; i--) {
+    vector<int> nextGreaterElements(vector<int>& nums) 
+    {
+        vector<int>ans(nums.size(),-1);
+        stack<int>st;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
             st.push(nums[i]);
         }
-
-        // Traverse the array from back to front, simulating circular behavior
-        for (int i = n - 1; i >= 0; i--) {
-            // Remove elements from the stack that are smaller or equal to the current element
-            while (!st.empty() && st.top() <= nums[i]) {
+        
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && st.top()<=nums[i])
+            {
                 st.pop();
             }
-
-            // If stack is not empty, the top element is the next greater element
-            if (!st.empty()) {
-                result[i] = st.top();
+            if(!st.empty())
+            {
+                ans[i]=st.top();
             }
-
-            // Push the current element into the stack
             st.push(nums[i]);
         }
-
-        return result;
+        return ans;
     }
 };
