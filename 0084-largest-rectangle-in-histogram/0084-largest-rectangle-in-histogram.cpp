42dc05a2dc4ab7efaad760dab51a98c7435bb0e6
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-23 21:13:25 +0530

    Time: 23 ms (60.61%), Space: 81.5 MB (58.14%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -4,33 +4,42 @@ public:
         int ans = 0;
         stack<int> st;
 
-        for (int i = 0; i < heights.size(); i++) {
-            while (!st.empty() && heights[i] < heights[st.top()]) {
-                int k = heights[st.top()];
+        for (int i = 0; i < heights.size(); i++) 
+        {
+            while (!st.empty() && heights[i] < heights[st.top()]) 
+            {
+                int element = heights[st.top()];
                 st.pop();
                 
-                int u;
-                if (st.empty()) {
-                    u = i * k;
-                } else {
-                    u = k * (i - st.top() - 1);
+                int area;
+                if (st.empty()) 
+                {
+                    area = i * element;
+                } 
+                else 
+                {
+                    area = element * (i- st.top() - 1);
                 }
-                ans = max(ans, u);
+                ans = max(ans, area);
             }
             st.push(i);
         }
 
-        while (!st.empty()) {
-            int k = heights[st.top()];
+        while (!st.empty()) 
+        {
+            int element = heights[st.top()];
             st.pop();
 
-            int u;
-            if (st.empty()) {
-                u = heights.size() * k;
-            } else {
-                u = k * (heights.size() - st.top() - 1);
+            int area;
+            if (st.empty()) 
+            {
+                area = heights.size() * element;
+            } 
+            else 
+            {
+                area = element * (heights.size() - st.top() - 1);
             }
-            ans = max(ans, u);
+            ans = max(ans, area);
         }
 
         return ans;
