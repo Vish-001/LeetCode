76dec8bed940717cd42f4c7d018d5a8f4bbfb4f2
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-24 00:09:31 +0530

    Time: 433 ms (5.04%), Space: 216.4 MB (5.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -6,24 +6,21 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) 
     {
-        multiset<int> st; // To maintain elements in the current window
+        multiset<int> st; 
         vector<int> ans;
 
-        // Initialize the first window
         for (int i = 0; i < k; i++) 
         {
             st.insert(nums[i]);
         }
 
-        // Add the maximum of the first window to the result
         ans.push_back(*st.rbegin());
 
-        // Slide the window
         for (int i = k; i < nums.size(); i++) 
         {
-            st.erase(st.find(nums[i - k])); // Remove the element that's sliding out of the window
-            st.insert(nums[i]); // Add the new element to the window
-            ans.push_back(*st.rbegin()); // Add the maximum of the current window
+            st.erase(st.find(nums[i - k]));
+            st.insert(nums[i]);
+            ans.push_back(*st.rbegin()); 
         }
 
         return ans;
