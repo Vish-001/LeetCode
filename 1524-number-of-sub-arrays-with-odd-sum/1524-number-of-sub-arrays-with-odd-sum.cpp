6674b3451b181cfeb856e850ed6ddbc3c0ea6926
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-02-25 12:05:20 +0530

    Time: 12 ms (23.62%), Space: 112.2 MB (26.71%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,34 +1,26 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
-    // subarray with odd no of 1
-    int numOfSubarrays(vector<int>& arr) 
-    {
-        vector<int>nums;
-        for(auto c:arr)
-        {
-            if(c%2==0)
-            {
-                nums.push_back(0);
-            }
-            else
-            {
-                nums.push_back(1);
-            }
-        }
+    int numOfSubarrays(vector<int>& arr) {
         unordered_map<int, int> freq;
         freq[0] = 1;  // Base case: empty prefix with even XOR
         int xorSum = 0, count = 0;
+        const int MOD = 1'000'000'007;  // Correct mod value
 
-        for (int num : nums) {
-            xorSum ^= num;  // Update prefix XOR
+        for (int num : arr) {
+            xorSum ^= num;  // XOR update directly on 0/1
 
             // If xorSum is odd, add number of even XORs seen before
             if (xorSum % 2 == 1) {
-                count += freq[0];
+                count = (count + freq[0]) % MOD;
             } 
             // If xorSum is even, add number of odd XORs seen before
             else {
-                count += freq[1];
+                count = (count + freq[1]) % MOD;
             }
 
             // Update frequency map
