95bd39c04621dac57f22e1001476b2e81b082796
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-03-04 02:03:12 +0530

    Time: 32 ms (74.49%), Space: 32.9 MB (15.88%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -2,42 +2,44 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) 
     {
-        if(hand.size()%groupSize!=0) return false;
+        if(hand.size() % groupSize != 0) return false;
 
-        unordered_map<int,int>mp;
-        for(auto c:hand)
-        {
+        unordered_map<int, int> mp;
+        for (auto c : hand) {
             mp[c]++;
         }
 
-        priority_queue<int>pq;
-        for(auto& c:mp)
-        {
+        // Use min-heap instead of max-heap
+        priority_queue<int, vector<int>, greater<int>> pq;  // Min heap
+        for (auto& c : mp) {
             pq.push(c.first);
         }
 
-        while(!pq.empty())
-        {
-            int prev=-1;
-            vector<int>temp;
-            for(int i=0;i<groupSize;i++)
-            {
-                if(pq.empty()) return false;
-                int t=pq.top();
-                mp[t]--;
-                temp.push_back(t);
-                cout<<prev<<"  "<<t<<endl;
-                if(prev!=-1 && prev!=t+1) return false;
-                prev=t;
+        while (!pq.empty()) {
+            int prev = -1;
+            vector<int> temp;
+
+            for (int i = 0; i < groupSize; i++) {
+                if (pq.empty()) return false;  // Not enough elements for a group
+
+                int t = pq.top();
                 pq.pop();
+
+                if (prev != -1 && prev + 1 != t) return false;  // Must be consecutive
+
+                mp[t]--;
+                if (mp[t] > 0) {
+                    temp.push_back(t);  // Store for reinsertion
+                } else {
+                    mp.erase(t);  // Clean up map
+                }
+
+                prev = t;
             }
 
-            for(auto c:temp)
-            {
-                if(mp[c]!=0)
-                {
-                    pq.push(c);
-                }
+            // Reinsert remaining elements back into the heap
+            for (auto c : temp) {
+                pq.push(c);
             }
         }
         return true;
