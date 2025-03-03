4ebbf76a56d69288b495346d1b71177eb51e665f
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-03-04 01:22:26 +0530

    Time: 37 ms (37.46%), Space: 47.9 MB (12.37%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        int freq[26] = {0};
-        for (auto c : tasks) {
-            freq[c - 'A']++;
-        }
-
-        priority_queue<int> pq;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                pq.push(freq[i]);
-            }
-        }
-
-        int ans = 0;
-        while (!pq.empty()) 
+    int leastInterval(vector<char>& tasks, int n) 
+    {
+        unordered_map<char,int>mp;
+        for(auto c:tasks)
         {
-            vector<int> temp;
-            int cnt = 0; 
-            for (int i = 0; i <= n; i++) 
-            { 
-                if (!pq.empty()) 
+            mp[c]++;
+        }
+        priority_queue<int>pq;
+
+        for(auto& p:mp)
+        {
+            pq.push(p.second);
+        }
+
+        int ans=0;
+        
+        while(!pq.empty())
+        {
+            vector<int>temp;
+            int cnt=0;
+            for(int i=0;i<=n;i++)
+            {
+                if(!pq.empty())
                 {
-                    int fq = pq.top();
+                    int freq=pq.top();
                     pq.pop();
-                    if (fq > 1) 
-                    {
-                        temp.push_back(fq - 1);
-                    }
+                    freq--;
                     cnt++;
+                    temp.push_back(freq);
                 }
             }
 
-            for (auto c : temp) 
+            for(auto c:temp)
             {
-                pq.push(c);
+                if(c>0)
+                {
+                    pq.push(c);
+                }
             }
 
-            if (!pq.empty()) 
+            if(!pq.empty())
             {
-                ans += n + 1;
-            } 
-            else 
+                ans+=n+1;
+            }
+            else
             {
-                ans += cnt;
+                ans+=cnt;
             }
         }
 
