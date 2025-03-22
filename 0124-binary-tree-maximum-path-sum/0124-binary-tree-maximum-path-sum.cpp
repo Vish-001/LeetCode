7f6d64197a566c7365c2075e164d1f039a22eb05
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-03-22 16:30:41 +0530

    Time: 3 ms (15.09%), Space: 28.2 MB (18.64%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int Chk(TreeNode* root, int& maxi) {
-        if (root == nullptr) return 0;
+    int computeBranchSum(TreeNode* root, int& maxSum) {
+        if (!root) return 0;
 
-        // Compute maximum path sum for left and right subtrees
-        int L = max(0, Chk(root->left, maxi)); // Ignore negative paths
-        int R = max(0, Chk(root->right, maxi)); // Ignore negative paths
+        int leftBranchSum = root->val + computeBranchSum(root->left, maxSum);
+        int rightBranchSum = root->val + computeBranchSum(root->right, maxSum);
 
-        // Update the global maximum path sum
-        maxi = max(maxi, L + R + root->val);
+        maxSum = max({maxSum,root->val,leftBranchSum,rightBranchSum,leftBranchSum + rightBranchSum - root->val});
 
-        // Return the maximum path sum including the current node as part of the path
-        return root->val + max(L, R);
+        return max({leftBranchSum, rightBranchSum, root->val});
     }
 
     int maxPathSum(TreeNode* root) {
-        int maxi = INT_MIN; 
-        Chk(root, maxi);
-        return maxi;
+        int maxSum = INT_MIN;
+        computeBranchSum(root, maxSum);
+        return maxSum;
     }
 };
